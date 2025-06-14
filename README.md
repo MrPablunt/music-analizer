bccb0a47ca81e05f64581e1419ac71aedfd97874
MrPablunt <123301432+MrPablunt@users.noreply.github.com>
2025-06-14 02:33:05 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Music Analizer
+
+Elegant. Modern. Relaxed.
---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+import './style.css';
+
+document.getElementById('app').innerHTML = `
+  <h1>Welcome to Music Analizer</h1>
+  <p>Elegant. Modern. Relaxed.</p>
+`;
