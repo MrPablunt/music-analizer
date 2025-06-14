1d81463efb7c2aeba7b14c716737e5bd82236274
MrPablunt <123301432+MrPablunt@users.noreply.github.com>
2025-06-14 02:28:20 -0600

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>🎵 Music Analizer</h1>
+      <p>Interfaz moderna, elegante y relajada.</p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
